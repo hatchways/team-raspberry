c8e892956771658be7a56c7e47072811f4430c8f
Kim Carey <kthomas06@gmail.com>
2020-07-20 22:17:32 -0700

    Change name to flask_app for clarity.

--- server/app.py
+++ server/app.py
@@ -1,9 +1,9 @@
+import json
+import os
 from flask import Flask
 from api.ping_handler import ping_handler
 from api.home_handler import home_handler
-
-import json
-import os
+# import views, models, resources
 
 # Configuration
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
@@ -12,10 +12,8 @@ GOOGLE_DISCOVERY_URL = (
     "https://accounts.google.com/.well-known/openid-configuration"
 )
 
+flask_app = Flask(__name__)
 
-app = Flask(__name__)
-
-
-app.register_blueprint(home_handler)
-app.register_blueprint(ping_handler)
+flask_app.register_blueprint(home_handler)
+flask_app.register_blueprint(ping_handler)
 
