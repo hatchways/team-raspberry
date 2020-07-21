9671d167183d21ff98227b67239b1ed8e67cb494
Kim Carey <kthomas06@gmail.com>
2020-07-20 22:17:32 -0700

    Change name to flask_app for clarity.

--- server/app.py
+++ server/app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from api.ping_handler import ping_handler
 from api.home_handler import home_handler
+# import views, models, resources
+
+flask_app = Flask(__name__)
 
 
-app = Flask(__name__)
-
-
-app.register_blueprint(home_handler)
-app.register_blueprint(ping_handler)
+flask_app.register_blueprint(home_handler)
+flask_app.register_blueprint(ping_handler)
 
