61b756f1484d6616b7233032664e1d8bd2c36de7
Kim Carey <kthomas06@gmail.com>
2020-07-21 13:10:31 -0700

    Revert "Add marshmallow package"
    This reverts commit eacbd0d9f0395667198fd1ab90176cc82b3764bf.

--- server/api/home_handler.py
+++ server/api/home_handler.py
@@ -5,4 +5,3 @@ home_handler = Blueprint('home_handler', __name__)
 @home_handler.route('/welcome')
 def welcome():
     return jsonify({'welcomeMessage': 'Step 1: Run the server (completed!)'})
-
--- server/app.py
+++ server/app.py
@@ -1,22 +1,11 @@
 from flask import Flask
-from flask_restful import Api
-
 from api.ping_handler import ping_handler
 from api.home_handler import home_handler
+# import views, models, resources
 
 flask_app = Flask(__name__)
-api = Api(flask_app)
-
-import views, models, resources
 
 
 flask_app.register_blueprint(home_handler)
 flask_app.register_blueprint(ping_handler)
 
-api.add_resource(resources.UserRegistration, '/registration')
-api.add_resource(resources.UserLogin, '/login')
-api.add_resource(resources.UserLogoutAccess, '/logout/access')
-api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
-api.add_resource(resources.TokenRefresh, '/token/refresh')
-api.add_resource(resources.AllUsers, '/users')
-api.add_resource(resources.SecretResource, '/secret')
