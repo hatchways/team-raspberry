4fa8aeb707aa916277112a458f4ef84a7f3cbfb0
Kim Carey <kthomas06@gmail.com>
2020-07-22 11:32:33 -0700

    Implement UserRegistration and UserLogin endpoints.

--- server/resources.py
+++ server/resources.py
@@ -9,26 +9,34 @@ parser.add_argument('password', help = 'This field cannot be blank', required=Tr
 class UserRegistration(Resource):
     def post(self):
         data = parser.parse_args()
+
+        # Check if username already exists
+        if UserModel.find_by_username(data['username']):
+            return {'message': 'User {} already exists'.format(data['username'])}, 409
+
         new_user = UserModel(
-            username = data['username'],
-            password = data['password']
+            username=data['username'],
+            password=data['password']
         )
         try:
             new_user.save_to_db()
-            return {
-                'message': 'User {} was created'.format(data['username'])
-            }
+            return {'message': 'User {} was created'.format(data['username'])}, 201
         except Exception as e:
             return {'message': 'Something went wrong'}, 500
-
-        # TODO: Need to raise 409 error if username already exists
 
 
 
 class UserLogin(Resource):
     def post(self):
         data = parser.parse_args()
-        return data
+        current_user = UserModel.find_by_username(data['username'])
+        if not current_user:
+            return {'message': 'User {} doesn\'t exist'.format(data['username'])}
+
+        if data['password'] == current_user.password:
+            return {'message': 'Logged in as {}'.format(current_user.username)}
+        else:
+            return {'message': 'Wrong credentials'}, 401
 
 class UserLogoutAccess(Resource):
     def post(self):
@@ -44,10 +52,10 @@ class TokenRefresh(Resource):
 
 class AllUsers(Resource):
     def get(self):
-        return {'message': 'List of users'}
+        return UserModel.return_all()
 
     def delete(self):
-        return {'message': 'Delete all users'}
+        return UserModel.delete_all()
 
 class SecretResource(Resource):
     def get(self):
