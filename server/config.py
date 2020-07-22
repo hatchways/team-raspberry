23c9c6243c9a6b3c49c9a2915572fb0aacdb99da
Kim Carey <kthomas06@gmail.com>
2020-07-21 21:58:11 -0700

    Config for db. Need to move to secrets.

--- server/config.py
+++ server/config.py
@@ -1,3 +1,5 @@
 import os
 
-TEAM_NAME = os.environ['TEAM_NAME']
+TEAM_NAME = os.environ.get('TEAM_NAME')
+# TODO: Move this into a secrets file or something that's not committed (unfortunately .env is).
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:hatchways@localhost/hatchways_crm'
