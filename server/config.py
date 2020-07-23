b1c9f282e280b757f18c6127419d4f0eae823555
Kim Carey <kthomas06@gmail.com>
2020-07-23 11:37:40 -0700

    Updated SECRET_KEY per Shums feedback.

--- server/config.py
+++ server/config.py
@@ -3,3 +3,4 @@ import os
 TEAM_NAME = os.environ.get('TEAM_NAME')
 # TODO: Move this into a secrets file or something that's not committed (unfortunately .env is).
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:hatchways@localhost/hatchways_crm'
+SECRET_KEY = os.environ.get('SECRET_KEY')
