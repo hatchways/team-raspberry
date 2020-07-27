c873cc47ac1cfdcbb545445fb6b132f96f912a3d
Kim Carey <kthomas06@gmail.com>
2020-07-27 10:51:57 -0700

    corrected database within URI

--- server/config.py
+++ server/config.py
@@ -3,8 +3,7 @@ import os
 TEAM_NAME = os.environ.get('TEAM_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
-# SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@localhost/crm_test'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:hatchways@localhost/hatchways_crm'
+SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@localhost/hatchways_crm'
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 BCRYPT_LOG_ROUNDS = 12
