de4a29943b1c94cc63fbfd1c322277577d533c45
KaminKevCrew <kaminskikeving@gmail.com>
2020-07-30 11:03:49 -0700

    Work on Campaign contents

--- client/src/components/CampainStep.js
+++ client/src/components/CampainStep.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import FormControl from "@material-ui/core/FormControl";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
-import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
+import TextField from "@material-ui/core/TextField";
+import TextEditor from "./TextEditor";
 
 
 export default function CampaignStep() {
   const classes = useStyles();
   const [stepName, setStepName] = useState("")
-  const [templates, setTemplates] = useState([])
   const [saved, setSaved] = useState(false)
-  const [selectedTemplate, setSelectedTemplate] = useState(
-    <MenuItem key="0">Select Template</MenuItem>
-  );
+  const [openEditor, setOpenEditor] = useState(false)
+
+  const handleEditorOpen = () => {
+    setOpenEditor(true)
+  }
+
+  const handleEditorClose = () => {
+    setOpenEditor(false)
+  }
 
   const handleEdit = () => {
     setSaved(false)
@@ -26,26 +29,6 @@ export default function CampaignStep() {
 
   const handleText = (e) => {
     setStepName(e.target.value)
-  }
-
-  const handleSelect = (e) => {
-    setSelectedTemplate(e.target.value)
-  }
-
-  // TODO: move to CampaignShow and pass in as props so only 1 http request needs to be made. As it is now, an HTTP request will be made each time a step is added.
-  const fetchTemplateTitles = useEffect(() => {
-    // fetch("/templates", {
-    //   method: "GET",
-    // }).then((response) => response.json().then(data => {
-    //   titlesToMenuItems(data.titles)
-    // }))
-  }, [])
-
-  const titlesToMenuItems = (templates) => {
-    templates.map((template) => {
-      return <MenuItem key={template} value="template">{template}</MenuItem>
-    })
-    setTemplates(templates)
   }
 
   const handleSubmit = (e) => {
@@ -62,28 +45,31 @@ export default function CampaignStep() {
           Step Name: {stepName}
         </CardContent>
         <CardContent className={classes.cardContent}>
-          Template: {selectedTemplate}
+          {/* Email draft goes here */}
         </CardContent>
         <CardActions>
           <Button onClick={handleEdit}>Edit</Button>
         </CardActions>
       </Card>
     :
-    <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
-      <FormControl>
-        <InputLabel htmlFor="step-name">Step Name:</InputLabel>
-        <Input id="step-name" value={stepName} onChange={handleText} className={classes.text}/>
-      </FormControl>
-      <FormControl>
-        <InputLabel htmlFor="template-select-label" id="template-select-label" >Select Template</InputLabel>
-        <Select labelid="template-select-label" id="template-select" value={selectedTemplate} onChange={handleSelect} className={classes.select}>
-          <MenuItem key="1" value="Template 1">Template 1</MenuItem>
-          <MenuItem key="2" value="Template 2">Template 2</MenuItem>
-          {templates}
-        </Select>
-      </FormControl>
-      <Button className={classes.button} label="Submit" type="submit">Save</Button>
-    </form>
+    <Card>
+      <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <CardContent>
+          <TextField label="Step Name" id="step-name" value={stepName} onChange={handleText} className={classes.text}></TextField>
+        </CardContent>
+        <CardActions>
+          <Button className={classes.button} onClick={handleEditorOpen}>
+            Create Email
+          </Button>
+          <TextEditor open={openEditor} onClose={handleEditorClose}/>
+        </CardActions>
+        <CardActions>
+          <Button className={classes.button} label="Submit" type="submit">
+            Save
+          </Button>
+        </CardActions>
+      </form>
+    </Card>
   )
 }
 
--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -1,37 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 import { tinyMCEKey } from "../APIKeys";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import PropTypes from "prop-types";
 
-export default function MyEditor() {
+export default function TextEditor(props) {
+  const classes = useStyles()
+  const [title, setTitle] = useState("")
+  const [subject, setSubject] = useState("")
+  const [content, setContent] = useState("")
+  let { onClose, selectedValue, open } = props
+
+
   const handleEditorChange = (content, editor) => {
     console.log("Content was updated:", content);
+    setContent(content)
   };
+
+  const handleClose = () => {
+    onClose(selectedValue)
+  }
   
-  const handleSave = (content) => {
-    
+  const handleSave = (e) => {
+    e.preventDefault()
+    console.log(e)
+    // const template = JSON.stringify({ title, subject, content });
+    // fetch("/api/templates", {
+    //   method: "POST",
+    //   data: template,
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // }).then((response) => response.json().then((data) => console.log(data)));
   }
 
   return(
-    <Editor
-      initialValue="<p>Initial content</p>"
-      apiKey={tinyMCEKey}
-      init={{
-        height: 300,
-        menubar: false,
-        plugins: [
-          "advlist autolink lists link image",
-          "charmap print preview anchor help",
-          "searchreplace visualblocks code",
-          "insertdatetime media table paste wordcount",
-        ],
-        toolbar:
-          "undo redo | formatselect | bold italic | \
-              alignleft aligncenter alignright | \
-              bullist numlist outdent indent | help",
-      }}
-      onChange={handleEditorChange}
-    />
+    <Dialog open={open}>
+      <DialogTitle>New Email</DialogTitle>
+      <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSave}>
+        <TextField key="subject" label="Email Subject" onChange={setSubject}></TextField>
+        <Editor
+          // initialValue="<p>Initial content</p>"
+          apiKey={tinyMCEKey}
+          init={{
+            height: 300,
+            menubar: false,
+            plugins: [
+              "advlist autolink lists link image",
+              "charmap print preview anchor help",
+              "searchreplace visualblocks code",
+              "insertdatetime media table paste wordcount",
+            ],
+            toolbar:
+              "undo redo | formatselect | bold italic | \
+                  alignleft aligncenter alignright | \
+                  bullist numlist outdent indent | help",
+          }}
+          onChange={handleEditorChange}
+        />
+        <Button label="Submit" type="submit">Save Template</Button>
+      </form>
+    </Dialog>
   )
 }
 
+TextEditor.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+
+  },
+  dialogTitle: {
+
+  },
+  title: {
+
+  },
+  subject: {
+
+  },
+  editor: {
+
+  },
+  button: {
+
+  }
+}))
