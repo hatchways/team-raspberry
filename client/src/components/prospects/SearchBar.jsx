77d1021f88c5d8b05ed0c12d40ce369d7753cdf9
Kim Carey <kthomas06@gmail.com>
2020-07-30 22:16:17 -0700

    Fetch prospects data from backend on load and get filering working.

--- client/src/components/prospects/SearchBar.jsx
+++ client/src/components/prospects/SearchBar.jsx
@@ -1,17 +1,15 @@
 import React, { useState } from "react";
-import Prospects from "../../pages/Prospects";
 import { makeStyles } from "@material-ui/core/styles";
-import CustomTextField from "../CustomTextField";
-import {Grid} from "@material-ui/core";
 
+export default function SearchBar(props) {
 
-export default function SearchBar() {
+  const handleSearchChange = (e) => {
+    props.setText(e.target.value);
+  };
+
   return (
     <input type="text" onChange={handleSearchChange} placeholder="Search" />
     );
 }
 
-const handleSearchChange = (e) => {
-    const target = e.target;
-    console.log(target.value);
-  };
+
--- client/src/components/prospects/Table.jsx
+++ client/src/components/prospects/Table.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import * as Auth from "../../services/auth-services"
+
 
 import {
     TableContainer,
@@ -18,48 +19,60 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(id, name, email) {
-  return { id, name, email };
-}
-
-// const rows = [
-// //   createData('1','Kim', 'kim@fakeemail.com'),
-// //   createData('2','Kim', 'kim@fakeemail.com'),
-// //   createData('3','Kim', 'kim@fakeemail.com'),
-// //   createData('4','Kim', 'kim@fakeemail.com'),
-// //   createData('5','Kim', 'kim@fakeemail.com')
-// // ];
-
-export default function ProspectsTable() {
+export default function ProspectsTable(props) {
   const classes = useStyles();
 
-  const [rows, setRows] = useState([
-    createData('1','Kim', 'kim@fakeemail.com')
-  ]);
+  const [rows, setRows] = useState([]);
+  const [filteredRows, setFilteredRows] = useState([]);
 
   function fetchProspects() {
     Auth.getProspects().then((res) => {
-      console.log(res.data);
       setRows(res.data.prospects);
+      setFilteredRows(res.data.prospects);
     });
+
   }
+
+  function filterProspects(query) {
+    if (query === '') {
+      return rows;
+    }
+    let filtered = [];
+    for(let i=0; i < rows.length; i++) {
+      let inFirst = rows[i].firstName.toLowerCase().includes(query.toLowerCase());
+      let inLast = rows[i].lastName.toLowerCase().includes(query.toLowerCase());
+      let inEmail = rows[i].email.toLowerCase().includes(query.toLowerCase());
+      if (inFirst || inLast || inEmail) {
+        filtered.push(rows[i]);
+      }
+    }
+    setFilteredRows(filtered);
+  }
+
+  // Gets called every time this component gets rendered (updated).
+  useEffect(() => {
+    filterProspects(props.text);
+  }, [props.text]);
+
+  useEffect(() => {
+      fetchProspects();
+  }, []);
 
   return (
     <TableContainer component={Paper}>
-      <button onClick={fetchProspects}>
-        Click me!
-      </button>
       <Table className={"stuff"} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Name</TableCell>
+            <TableCell>First Name</TableCell>
+            <TableCell>Last Name</TableCell>
             <TableCell>Email</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {filteredRows.map((row) => (
             <TableRow key={row.id}>
-              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.firstName}</TableCell>
+              <TableCell>{row.lastName}</TableCell>
               <TableCell>{row.email}</TableCell>
             </TableRow>
           ))}
