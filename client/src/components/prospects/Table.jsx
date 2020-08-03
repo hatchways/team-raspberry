d9ac9269aaf10a182c89028fb0ded76cdc7b741a
Kim Carey <kthomas06@gmail.com>
2020-08-03 15:14:09 -0700

    Implement checkbox functionality on prospects table so that checkbox toggles on/off based on when user clicks.

--- client/src/components/prospects/Table.jsx
+++ client/src/components/prospects/Table.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import * as Auth from "../../services/auth-services"
-
+import Checkbox from "./Checkbox";
 
 import {
     TableContainer,
@@ -27,10 +27,33 @@ export default function ProspectsTable(props) {
 
   function fetchProspects() {
     Auth.getProspects().then((res) => {
-      setRows(res.data.prospects);
-      setFilteredRows(res.data.prospects);
+      let pList = res.data.prospects;
+      for (let i=0; i < pList.length; i++) {
+        // Set the checkbox default value in the row state so it can be accessed easily by campaigns.
+        pList[i]['checked'] = false;
+      }
+      setRows(pList);
+      setFilteredRows(pList);
     });
+  }
 
+  function handleCheck(isChecked, id) {
+    // 'rows' is read-only, so we need to copy everything and then change.
+    // See https://stackoverflow.com/a/49502115
+    for (let i=0; i < rows.length; i++) {
+      if (rows[i]['id'] === id) {
+        // 1. Make a shallow copy of the items
+        let items = [...rows];
+        // 2. Make a shallow copy of the item you want to mutate
+        let item = {...items[i]};
+        // 3. Replace the property you're intested in
+        item.checked = isChecked;
+        // 4. Put it back into our array. N.B. we *are* mutating the array here, but that's why we made a copy first
+        items[i] = item;
+        // 5. Set the state to our new copy
+        setRows(items);
+      }
+    }
   }
 
   function filterProspects(query) {
@@ -58,11 +81,17 @@ export default function ProspectsTable(props) {
       fetchProspects();
   }, []);
 
+  // //Helpful to debug rows state when it changes.
+  // useEffect(() => {
+  //     console.log(rows);
+  // }, [rows]);
+
   return (
     <TableContainer component={Paper}>
       <Table className={"stuff"} aria-label="simple table">
         <TableHead>
           <TableRow>
+            <TableCell>Select</TableCell>
             <TableCell>First Name</TableCell>
             <TableCell>Last Name</TableCell>
             <TableCell>Email</TableCell>
@@ -71,6 +100,7 @@ export default function ProspectsTable(props) {
         <TableBody>
           {filteredRows.map((row) => (
             <TableRow key={row.id}>
+              <TableCell><Checkbox checked={row.checked}  clickEvent={(isChecked) => handleCheck(isChecked, row.id)}/></TableCell>
               <TableCell>{row.firstName}</TableCell>
               <TableCell>{row.lastName}</TableCell>
               <TableCell>{row.email}</TableCell>
