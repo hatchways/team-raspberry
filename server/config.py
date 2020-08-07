5c4a8b3b71e15bdc84f4cf6e084befc7f87d5473
AlwaysHunt <37853845+AlwaysHunt@users.noreply.github.com>
2020-08-06 22:40:16 -0400

    Set up the headers for axios right away

--- server/config.py
+++ server/config.py
@@ -3,7 +3,7 @@ import os
 TEAM_NAME = os.environ.get('TEAM_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
-SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@localhost/crm_test'
+SQLALCHEMY_DATABASE_URI = f'postgresql://postgres:123456789@localhost/crm_test'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 BCRYPT_LOG_ROUNDS = 12
 
